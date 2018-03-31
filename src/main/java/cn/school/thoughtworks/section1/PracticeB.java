3dac38a13d0d12aec3ef327cf990e59dbf9e0812
summer2023 <happyxia1023@163.com>
2018-03-31 11:58:11 +0800

    section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,21 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
+        List<String> result = new ArrayList<String>();
+        for (String str:collection1
+             ) {
+            for (int i = 0; i < collection2.size(); i++) {
+                if (collection2.get(i).contains(str)) {
+                    result.add(str);
+                }
+            }
+        }
+
         return null;
     }
 }
--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,24 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
+        List<String> result = new ArrayList<String>();
+        List<List<String>>  valls = new ArrayList<List<String>>(collection2.values());
+        for (String str:collection1) {
+            for (int i = 0; i < valls.size(); i++) {
+                if (valls.get(i).contains(str)) {
+                    result.add(str);
+                }
+
+            }
+
+        }
         return null;
     }
 }
